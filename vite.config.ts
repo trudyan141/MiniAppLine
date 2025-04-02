38c67c1992b65b14d5f3a3a0853c63cedc5cab72
trunglk-ibl <trunglk@blockchainlabs.asia>
2025-04-02 13:17:48 +0700

    update buidl again

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: "/MiniAppLine/", 
   plugins: [
     react(),
     runtimeErrorOverlay(),
